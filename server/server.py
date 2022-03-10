b3c3e7125b33c8fc5b56b07758267007dda359e4
LeonPratt <leon.alex.pratt@gmail.com>
2022-03-10 22:43:40 +0000

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,66 +1,77 @@
 import socket
+import time
+
+while True:
+    manage_to_send =False
+
+    try:
+
+        def send():
+            room = b"Kitchen(button)"
+            message = b"PlaySound:" + room
+            TCP_IP = "192.168.1.20"
+            TCP_PORT = 8080
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((TCP_IP, TCP_PORT))
+            s.send(message)
+            data = s.recv(1024).decode()
+            print("recieved:", data)
+            if data == "PlaySound:Kitchen(button)":
+                manage_to_send = True
+            s.close()
 
 
-try:
+        #def send_UDP():
+        #    message_toSend = "PlaySound"
+        #    bytesToSend = str.encode(message_toSend)
+    #
+        #    ServerAdressPort = ("192.168.1.20", 8080)
+    #
+        #    UDPclientsocket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
+    #
+        #    UDPclientsocket.sendto(bytesToSend, ServerAdressPort)
 
-    def send():
-        message = "PlaySound"
-        
-        TCP_IP = "192.168.1.20"
-        TCP_PORT = 8080
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
-        s.send(b'PlaySound' )
-        data = s.recv(1024)
-        s.close()
-        print("recieved:", data)
+        def webserver():
+            SERVER_HOST = '0.0.0.0'
+            SERVER_PORT = 8000
 
-    #def send_UDP():
-    #    message_toSend = "PlaySound"
-    #    bytesToSend = str.encode(message_toSend)
-#
-    #    ServerAdressPort = ("192.168.1.20", 8080)
-#
-    #    UDPclientsocket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
-#
-    #    UDPclientsocket.sendto(bytesToSend, ServerAdressPort)
+            # Create socket
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.bind((SERVER_HOST, SERVER_PORT))
+            server_socket.listen(1)
+            print('Listening on port %s ...' % SERVER_PORT)
 
-    def webserver():
-        SERVER_HOST = '0.0.0.0'
-        SERVER_PORT = 8000
+            while True:   
+                # Wait for client connections
+                client_connection, client_address = server_socket.accept()
+                
 
-        # Create socket
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind((SERVER_HOST, SERVER_PORT))
-        server_socket.listen(1)
-        print('Listening on port %s ...' % SERVER_PORT)
+                # Get the client request
+                request = client_connection.recv(1024).decode()
+                print("request = %s"  % request)
+                Spltrequest = request.split("\n")[0]
+                Spltrequest =Spltrequest.split("/")[1]
+                print(Spltrequest)
+                if Spltrequest == "single HTTP":
+                    send()
+                
 
-        while True:   
-            # Wait for client connections
-            client_connection, client_address = server_socket.accept()
-            
-
-            # Get the client request
-            request = client_connection.recv(1024).decode()
-            print("request = %s"  % request)
-            
-            if request != "":
-                send()
-
-            # Send HTTP response
-            response = 'HTTP/1.0 200 OK\n\nHello World'
-            client_connection.sendall(response.encode())
-            client_connection.close()
+                # Send HTTP response
+                if manage_to_send:
+                    response = 'HTTP/1.0 200 OK\n\nHello World'
+                    client_connection.sendall(response.encode())
+                    client_connection.close()
 
 
+        print ("send.py in main")
 
-    print ("send.py in main")
+        webserver()
+    except Exception as e:
+        f = open("errorlog.txt", "a")
+        msg = e
+        f.write(str(e))
+        f.close()
 
-    #webserver()
-except Exception as e:
-    f = open("errorlog.txt", "a")
-    msg = e
-    f.write(str(e))
-    f.close()
-
+ 
+    time.sleep(4)
